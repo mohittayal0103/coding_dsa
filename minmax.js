faee0d32412433770564e3c4725a9f7d4c5a72ff
Mohit Tayal <mohittayal0103@gmail.com>
2024-01-31 01:13:39 +0530

    2.hackerrank

---
+++ minmax.js
@@ -0,0 +1,26 @@
+// Problem link
+// https://www.hackerrank.com/challenges/three-month-preparation-kit-mini-max-sum/problem?isFullScreen=true&h_l=interview&playlist_slugs%5B%5D=preparation-kits&playlist_slugs%5B%5D=three-month-preparation-kit&playlist_slugs%5B%5D=three-month-week-one
+
+for (let i = 0; i < arr.length;) {
+  if (arr[i] > arr[i + 1]) {
+      let a = arr[i];
+      arr[i] = arr[i + 1];
+      arr[i + 1] = a;
+      i--;
+  }
+  else {
+      i++;
+  }
+  }
+  
+  let min = 0;
+  let max = 0;
+  
+  for(let i = 0; i<arr.length-1; i++){
+      min = min+arr[i];
+  }
+  for(let i = 1; i<arr.length; i++){
+      max = max+arr[i];
+  }
+  
+  console.log(min, max);
