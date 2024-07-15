930c3159f5566e36eabd153cf9a3eee5dbc79407
Mohit Tayal <158062056+mohittayal0103@users.noreply.github.com>
2024-07-15 16:16:41 +0530

    Power of Two code Optimised

--- poweroftwo.js
+++ poweroftwo.js
@@ -19,3 +19,22 @@ console.log(isPowerOfTwo(4));
 console.log(isPowerOfTwo(5));
 
 //BigO -> O(logn)   : Value of n is divided by half
+
+
+
+//-----------------------------
+
+function isPowerOfTwoBitWise(n){
+    if(n < 1){
+        return false;
+    }
+    return (n & (n-1)) === 0
+}
+
+console.log(isPowerOfTwoBitWise(1));
+console.log(isPowerOfTwoBitWise(2));
+console.log(isPowerOfTwoBitWise(3));
+console.log(isPowerOfTwoBitWise(4));
+console.log(isPowerOfTwoBitWise(5));
+
+//BigO -> O(1)
