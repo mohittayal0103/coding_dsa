2d731b348ccbee4c86408f23762b70e169703229
Mohit Tayal <158062056+mohittayal0103@users.noreply.github.com>
2024-07-15 16:03:02 +0530

    Prime Number code

---
+++ primenumber.js
@@ -0,0 +1,17 @@
+function isPrime(n){
+    if(n < 2){
+        return false;
+    }else{
+        for(let i = 2; i < n; i++){
+            if(n % i === 0){
+                return false;
+            }else{
+                return true;
+            }
+        }
+    }
+}
+
+console.log(isPrime(1));
+console.log(isPrime(5));
+console.log(isPrime(4));
