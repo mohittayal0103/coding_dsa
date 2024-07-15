e9015a4dc36334a263643f85f6d4153030ef6d59
Mohit Tayal <158062056+mohittayal0103@users.noreply.github.com>
2024-07-15 16:07:57 +0530

    Prime Number code

--- primenumber.js
+++ primenumber.js
@@ -2,16 +2,18 @@ function isPrime(n){
     if(n < 2){
         return false;
     }else{
-        for(let i = 2; i < n; i++){
+        for(let i = 2; i <= Math.sqrt(n); i++){
             if(n % i === 0){
                 return false;
-            }else{
-                return true;
             }
         }
     }
+    return true;
 }
 
 console.log(isPrime(1));
 console.log(isPrime(5));
 console.log(isPrime(4));
+
+//BigO -> O (sqrt(n))
+
