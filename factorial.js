6e4a650ed80d129c5f8ea7fcf8a6d34b6bd9ec59
Mohit Tayal <158062056+mohittayal0103@users.noreply.github.com>
2024-07-15 15:58:03 +0530

    Factorial code

---
+++ factorial.js
@@ -0,0 +1,15 @@
+function factorial(n){
+    let fact = 1;
+        for(let i = 2; i <= n; i++){
+            fact = fact * i;
+        }
+
+        return fact;
+}
+
+console.log(factorial(0));
+console.log(factorial(1));
+console.log(factorial(2));
+console.log(factorial(3));
+console.log(factorial(4));
+console.log(factorial(5));
